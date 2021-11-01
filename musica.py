5b9a9644f9a96e5253426dc19272f99b6d934461
nabm <nathabmmb@gmail.com>
2021-11-01 13:11:33 -0300

    implementa __str__

--- musica.py
+++ musica.py
@@ -1,8 +1,11 @@
 class Musica:
+    _generos = ["Balada", "Pop", "Rock", "Folclore", "Electrónica", "Otros"]
+    _idiomas = ["Español", "Inglés", "Francés", "Portugués", "Otros"]
+
     def __init__(self, titulo, genero, idioma):
         self.titulo = titulo
         self.genero = genero
         self.idioma = idioma
 
     def __str__(self):
-        return "{}  ({}/{})".format(self.titulo, self.genero, self.idioma)
+        return "{}  ({} - {})".format(self.titulo, self._generos[self.genero], self._idiomas[self.idioma])
